917634718dab7207de7ebaa02033bc8c6bab5cfc
Jess Robinson <castaway@desert-island.me.uk>
2011-12-03 14:35:38 +0000

    Figured out how to get the HTML::Zoom bits to work with DBIC for the front_page announcements list

--- lib/PresentingPerl/Schema/Result/Bucket.pm
+++ lib/PresentingPerl/Schema/Result/Bucket.pm
@@ -102,6 +102,10 @@ __PACKAGE__->has_many(
 # Created by DBIx::Class::Schema::Loader v0.07014 @ 2011-12-03 12:17:58
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:wBNqjqvZqIksnTrjVNsO/w
 
+sub video_count {
+    my ($self) = @_;
+    return $self->videos->count;
+}
 
 # You can replace this text with custom code or comments, and it will be preserved on regeneration
 1;
--- lib/PresentingPerl/Web/View/Zoom/Root.pm
+++ lib/PresentingPerl/Web/View/Zoom/Root.pm
@@ -7,7 +7,22 @@ sub front_page {
     my ($self, $stash) = @_;
 
     my $announcements = $stash->{announcements};
-    $_->select('#announcement-list')->replace_content("YooHoo!");
+    my $ann_list = [ map { 
+        my $obj = $_; 
+        sub {
+            $_->select('.bucket-name')->replace_content($obj->bucket->name)
+                ->select('.made-at')->replace_content($obj->made_at->iso8601())
+                ->select('.bucket-link')->set_attribute(
+                'href' => $obj->bucket->slug.'/'
+                )
+#               ->select('.new-videos')->replace_content($obj->video_count)
+                ->select('.total-videos')->replace_content(
+                $obj->bucket->video_count
+                )
+        }
+                     } $announcements->all ];
+
+    $_->select('#announcement-list')->repeat_content($ann_list);
 
 
 
